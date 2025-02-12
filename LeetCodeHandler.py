fa27e53098084d39a2b410f2721719b65900e958
vidishraj <vidishraj@gmail.com>
2025-02-12 13:47:41 +0530

    feat: minot refacto and error handling

--- LeetCodeHandler.py
+++ LeetCodeHandler.py
@@ -25,14 +25,14 @@ class Leetcode:
         if countResponse.status_code != 200:
             self.Logger.error(f"Error from Leetcode while fetching problem count. Check cookies or internet! \n"
                               f"Status Code: {countResponse.status_code}")
-            return None  # Explicitly return None
+            exit(0)  # Explicitly return None
         jsonResponse = countResponse.json()
         # We want to fetch all the questions at once
         dfDict = self.convert_to_defaultdict(jsonResponse)
         limit = dfDict['data']['userProgressQuestionList']['totalNum']
         if limit is None or limit == 0:
             logging.error("No questions available to fetch. Are your filters okay?")
-            return
+            exit(0)
         limit += 1
         skip = 0
         problemsQuery = self.apiHandler.getProblemListQuery(difficulty, skip, limit)
@@ -40,7 +40,46 @@ class Leetcode:
         if problemListResponse.status_code != 200:
             self.Logger.error(f"Error from Leetcode while fetching problem list. Check cookies or internet! \n"
                               f"Status Code: {problemListResponse.status_code}")
-            return None  # Explicitly return None
+            exit(0)  # Explicitly return None
         jsonResponse = problemListResponse.json()
         problemListResponseDict = self.convert_to_defaultdict(jsonResponse)
-        return problemListResponseDict
+        questionList = problemListResponseDict['data']['userProgressQuestionList']['questions']
+        if questionList is None:
+            self.Logger.error("Error fetching final question list. Response status was okay. Is response mangled?")
+            exit(0)
+        return questionList
+
+    def submissionListForQuestion(self, questionSlug):
+        startOffset = 0
+
+        def checkSubmissionResponse(response):
+            if response.status_code != 200:
+                self.Logger.error(f"Error from Leetcode while fetching submission list. Check cookies or internet or "
+                                  f"question slug! \n"
+                                  f"Status Code: {response.status_code}")
+                exit(0)
+            return True
+
+        submissionListQuery = self.apiHandler.getSubmissionListQuery(questionSlug, startOffset)
+        submissionListResponse = self.apiHandler.makeRequest(submissionListQuery)
+        if not checkSubmissionResponse(submissionListResponse): return
+        submissionResponseJson = submissionListResponse.json()
+        submissionResponseDict = self.convert_to_defaultdict(submissionResponseJson)
+        while checkSubmissionResponse(submissionListResponse):
+            hasNext = submissionResponseDict['data']['questionSubmissionList']['hasNext']
+            submissions = submissionResponseDict['data']['questionSubmissionList']['submissions']
+            if hasNext is None or submissions is None:
+                self.Logger.error("Submission List response is mangled.")
+                exit(0)
+            for submission in submissions:
+                submission = self.convert_to_defaultdict(submission)
+                if submission['status'] == 10:
+                    self.Logger.info(f"Submission found for question {submission}")
+                    return submission['id']
+            if not hasNext:
+                break
+            # Make next request
+            startOffset += 20
+            submissionListQuery = self.apiHandler.getSubmissionListQuery(questionSlug, startOffset)
+            submissionListResponse = self.apiHandler.makeRequest(submissionListQuery)
+        self.Logger.error(f"Could not find a submission for question slug {questionSlug}")
